38da566d5aef82cb9a470907e003b0536ab638a1
realrobertdouglas <robert.douglas@hotmail.com>
2021-08-18 21:07:44 -0500

    will page

---
+++ js/mark.9fdeea.js
@@ -0,0 +1,19 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+var wl;
+
+!function(){if("Promise"in window){var e,t,r=document,n=function(){return r.createElement("link")},o=new Set,a=n(),s=a.relList&&a.relList.supports&&a.relList.supports("prefetch"),i=location.href.replace(/#[^#]+$/,"");o.add(i);var c=function(e){var t=location,r="http:",n="https:";if(e&&e.href&&e.origin==t.origin&&[r,n].includes(e.protocol)&&(e.protocol!=r||t.protocol!=n)){var o=e.pathname;if(!(e.hash&&o+e.search==t.pathname+t.search||"?preload=no"==e.search.substr(-11)||".html"!=o.substr(-5)&&".html"!=o.substr(-5)&&"/"!=o.substr(-1)))return!0}},u=function(e){var t=e.replace(/#[^#]+$/,"");if(!o.has(t)){if(s){var a=n();a.rel="prefetch",a.href=t,r.head.appendChild(a)}else{var i=new XMLHttpRequest;i.open("GET",t,i.withCredentials=!0),i.send()}o.add(t)}},p=function(e){return e.target.closest("a")},f=function(t){var r=t.relatedTarget;r&&p(t)==r.closest("a")||e&&(clearTimeout(e),e=void 0)},l={capture:!0,passive:!0};r.addEventListener("touchstart",function(e){t=performance.now();var r=p(e);c(r)&&u(r.href)},l),r.addEventListener("mouseover",function(r){if(!(performance.now()-t<1200)){var n=p(r);c(n)&&(n.addEventListener("mouseout",f,{passive:!0}),e=setTimeout(function(){u(n.href),e=void 0},80))}},l)}}();
+
+$(function(){
+r=function(){dpi=window.devicePixelRatio;};
+if(!window.HTMLPictureElement){r();}
+wl=new woolite();
+wl.init();
+wl.addAnimation($('.js3')[0], "1.00s", "2.00s", 1, 100);
+wl.addAnimation($('.js4')[0], "1.00s", "3.00s", 1, 100);
+wl.addAnimation($('.js5')[0], "1.00s", "3.00s", 1, 100);
+wl.addAnimation($('.js6')[0], "1.00s", "5.00s", 1, 100);
+wl.addAnimation($('.js7')[0], "1.00s", "6.00s", 1, 100);
+wl.addAnimation($('.js8')[0], "1.00s", "7.00s", 1, 100);
+wl.start();
+
+});
